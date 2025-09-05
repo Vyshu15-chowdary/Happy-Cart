9cabcc9f7451a461e669431b2317ed959b03bfc0
vyshnavi <vishuchowdary20@gmail.com>
2025-09-05 18:39:36 +0530

    Mapping of cartproducts

---
+++ Frontend/components/Cartitem.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Cart = ({cart})=>{
+
+    const total = cart.reduce((sum,item)=> sum + item.price,0);
+
+    return(
+        <div>
+            <h2>🛒 Cart</h2>
+            {cart.length === 0 ? (
+                <p>No items in the cart</p>
+            ):(
+                <ul>
+                    {cart.map((item,i)=>(
+                        <li key={i} className="flex justify-between border-b py-2">
+                            <span>{item.name}</span>
+                            <span>₹{item.price}</span>
+
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </div>
+    )
+
+}
